a2062335e21397bac3110cae657f2b3aef196637
Leandro Pereira <leandronobper@gmail.com>
2024-11-08 18:13:41 -0300

    Agregar comentarios en PokemonAdapter, DicPokemonTypeStates, DicTypeEffectivity, DiscordBot y DiscordCommands. Solucionar bug que no baneaba pokemons con menos de 4 moves.

--- src/Library/DiscordBot/DiscordBot.cs
+++ src/Library/DiscordBot/DiscordBot.cs
@@ -3,11 +3,31 @@ using DSharpPlus.SlashCommands;
 
 namespace Library.DiscordBot
 {
+    /// <summary>
+    /// Clase para la inicialización y configuración del bot de Discord.
+    /// Utiliza DSharpPlus para conectarse a Discord y permitir utilizar comandos.
+    /// </summary>
     public class DiscordBot
     {
+        /// <summary>
+        /// Cliente de Discord utilizado para conectar y gestionar el bot.
+        /// </summary>
         private DiscordClient Client;
+        
+        /// <summary>
+        /// Extensión de comandos de barra para manejar interacciones de comandos con el bot.
+        /// </summary>
         private SlashCommandsExtension SlashCommands;
-
+        
+        /// <summary>
+        /// Método principal que configura y conecta el bot de Discord.
+        /// </summary>
+        /// <remarks>
+        /// Obtiene el token del bot desde la variable de entorno <c>DISCORD_TOKEN</c> y configura
+        /// el cliente de Discord utilizando <c>DiscordConfiguration</c> proporcionada por <c>DSharpPlus</c>
+        /// y aplica los comandos del bot de discord pasandole a <c>SlashCommands</c> la clase <c>DiscordCommands</c>.
+        /// </remarks>
+        /// <exception cref="Exception">Lanzada cuando no se encuentra el token del bot en la variable de entorno <c>DISCORD_TOKEN</c>.</exception>
         public async Task Iniciate()
         {
             var token = Environment.GetEnvironmentVariable("DISCORD_TOKEN") 
