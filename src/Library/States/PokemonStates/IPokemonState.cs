52085ab610d5f0840046f74f9f98e770ba1cc1e0
Leandro Pereira <leandronobper@gmail.com>
2024-11-08 20:09:37 -0300

    Agregar la documentación a todas las clases que están en States.

--- src/Library/States/PokemonStates/IPokemonState.cs
+++ src/Library/States/PokemonStates/IPokemonState.cs
@@ -1,11 +1,61 @@
 namespace Library.States;
 
+/// <summary>
+/// Define la interfaz para los estados de un Pokémon, especificando
+/// los métodos y propiedades comunes para aplicar efectos de estado.
+/// Algunos métodos tiene valores por defecto.
+/// </summary>
 public interface IPokemonState
 {
+    /// <summary>
+    /// Obtiene el nombre del estado de pokemon.
+    /// </summary>
+    /// <returns>
+    ///     <c>EnumState</c> con el nombre del estado.
+    /// </returns>
     public EnumState Name { get; }
+    
+    /// <summary>
+    /// Obtiene el emoji que representa el estado del pokemon.
+    /// </summary>
+    /// /<returns>
+    ///     <c>string</c> que contiene un emoji que representa el estado del pokemon.
+    /// </returns>
     public string Emoji { get; }
+    
+    /// <summary>
+    /// Aplica el efecto dependiendo de la clase que se haya aplicado la interfaz.
+    /// Por defecto, no aplica ningun efecto.
+    /// </summary>
+    /// <param name="currentPokemon"><c>Pokemon</c> al que se le aplica el efecto.</param>
     public void ApplyEffect(Pokemon currentPokemon) { }
+    
+    /// <summary>
+    /// Método que devuelve los turnos restantes que estará bajo el efecto el pokemon.
+    /// </summary>
+    ///  <returns>
+    ///     <c>int</c> turnos restantes que estará bajo el efecto.
+    ///     Por defecto, <c>-1</c>. Si el valor es <c>-1</c> representa que el estado es permanente.
+    /// </returns>
     public int GetRemainingTurnsWithEffect() => -1;
+    
+    /// <summary>
+    /// Método que sirve para verificar si el pokemon puede atacar.
+    /// </summary>
+    /// <returns>
+    ///     <para><c>true</c> si puede atacar.</para>
+    ///     <para><c>false</c> si no puede atacar.</para>
+    ///     <para> Por defecto, <c>true</c>.</para>
+    /// </returns>
     public bool CanAttack() => true;
+    
+    /// <summary>
+    /// Método que sirve para verificar si el pokemon puede atacar.
+    /// </summary>
+    /// <returns>
+    ///     <para><c>true</c> si ha perdido el turno.</para>
+    ///     <para><c>false</c> si no ha peridod el turno.</para>
+    ///     <para> Por defecto, <c>false</c>.</para>
+    /// </returns>
     public bool HasLostTurn() => false;
 }
--- src/Library/States/PokemonStates/Paralyze.cs
+++ src/Library/States/PokemonStates/Paralyze.cs
@@ -1,15 +1,40 @@
 namespace Library.States;
 
+/// <summary>
+/// Clase que representa el estado <c>Paralyze</c> de un pokemon.
+/// </summary>
 public class Paralyze : IPokemonState
 {
+    /// <summary>
+    /// Permite obtener el nombre del estado.
+    /// </summary>
+    /// <returns>
+    ///     <c>EnumState</c> con el nombre del estado.
+    /// </returns>
     public EnumState Name { get; }
+    
+    /// <summary>
+    /// Obtiene un string con un emoji relacionado al estado <c>Paralyze</c>.
+    /// </summary>
+    /// <returns>
+    ///     <c>string</c> que incluye un emoji relacionado al estado.
+    /// </returns>
     public string Emoji { get; } = "\u26a1";
 
+    /// <summary>
+    /// Constructor de la clase <c>Paralyze</c> que inicializa el nombre del estado como <c>EnumState.Paralyze</c>.
+    /// </summary>
     public Paralyze()
     {
         Name = EnumState.Paralyze;
     }
 
+    /// <summary>
+    /// Método que permite saber si se ha perdido un turno de forma aleatoria con un 50% de probabilidad.
+    /// </summary>
+    /// <returns>
+    ///     <c>bool</c> que indica si se perdió el turno.
+    /// </returns>
     public bool HasLostTurn()
     {
         return new Random().Next(2) == 0;
