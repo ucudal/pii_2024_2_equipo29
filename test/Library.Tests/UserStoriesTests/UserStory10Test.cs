306496455d621790d2b6ac0f2bc6f188d3abab82
Matias Perez <matiperezgordano@gmail.com>
2024-11-08 23:19:56 -0300

    User stories Test Added

--- test/Library.Tests/UserStoriesTests/UserStory10Test.cs
+++ test/Library.Tests/UserStoriesTests/UserStory10Test.cs
@@ -1,15 +1,11 @@
-using DSharpPlus.Entities;
-using Library.DiscordBot;
-using Moq;
-
 namespace Library.Tests.UserStoriesTests;
 
-public class UserStory9Test
+public class UserStory10Test
 {
-    // 9. Como entrenador, quiero unirme a la lista de jugadores esperando por un oponente.
+    // 10. Como entrenador, quiero ver la lista de jugadores esperando por un oponente.
     // Criterios de aceptación:
-    // El jugador recibe un mensaje confirmando que fue agregado a la lista de espera.
-
+    // En la pantalla se ve la lista de jugadores que se unieron a la lista de espera.
+    
     // En nuestro caso no es posible simular este test, esto debido a que no podemos crear una instancia de DiscordMember desde 0
     // A su vez es necesario que sea una lista de DiscordMembers debido a que implementamos un sistema que crea una sala privada
     // para los usuarios que se encuentran en lista de espera. Esto sucede automaticamente al llegar al maximo de jugadores permitidos
@@ -21,8 +17,9 @@ public class UserStory9Test
     
     // Estan invitamos a participar en una batalla de practica.
     
+    
     [Test]
-    public void AddToWaitListTest()
+    public void ShowWaitListTest()
     {
         Assert.Pass();
     }
--- test/Library.Tests/UserStoriesTests/UserStory11Test.cs
+++ test/Library.Tests/UserStoriesTests/UserStory11Test.cs
@@ -1,19 +1,16 @@
-using DSharpPlus.Entities;
-using Library.DiscordBot;
-using Moq;
-
 namespace Library.Tests.UserStoriesTests;
 
-public class UserStory9Test
+public class UserStory11Test
 {
-    // 9. Como entrenador, quiero unirme a la lista de jugadores esperando por un oponente.
+    // 11. Como entrenador, quiero iniciar una ballata con un jugador que está esperando por un oponente.
     // Criterios de aceptación:
-    // El jugador recibe un mensaje confirmando que fue agregado a la lista de espera.
-
+    // Ambos jugadores son notificados del inicio de la batalla
+    // El jugador que tiene el primer turno se determina aleatoriamente.
+    
     // En nuestro caso no es posible simular este test, esto debido a que no podemos crear una instancia de DiscordMember desde 0
     // A su vez es necesario que sea una lista de DiscordMembers debido a que implementamos un sistema que crea una sala privada
     // para los usuarios que se encuentran en lista de espera. Esto sucede automaticamente al llegar al maximo de jugadores permitidos
-    // en la clase Game. A su vez ambos jugadores son notificados en el lobby y mediante un mensaje privado, con su respectivo link a 
+    // en la clase Game. A su vez ambos jugadores son notificados en el lobby mediante un mensaje privado, con su respectivo link a 
     // la sala de battalla.
     
     // A su vez en nuestro caso el Bot de Discord se encuentra en funcionamiento para esta entrega y esta especificado en el README.md, 
@@ -22,7 +19,7 @@ public class UserStory9Test
     // Estan invitamos a participar en una batalla de practica.
     
     [Test]
-    public void AddToWaitListTest()
+    public void StartBattleTest()
     {
         Assert.Pass();
     }
